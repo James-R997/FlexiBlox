4b426fc294f4b5a2fa7e788d81ebc9be893c14ff
JAMES-R997 <nooneshere001@proton.me>
2025-03-06 05:37:13 +0300

    imported the utils module and removed json from fxbx.py

--- fxbx.py
+++ fxbx.py
@@ -1,13 +1,5 @@
 #!/bin/python3
 
 from data.objects import Schedule
-from json import load, dump, JSONDecodeError
-
-
-try:
-    with open("schedules.json", "r") as file:
-        userSchedule = load(file)
-
-except JSONDecodeError:
-
+from utils.jsonHandler import loadSchedule, dumpSchedule
 
